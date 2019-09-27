9387a0e32d0b4a21b1e48e0d019f758d00df12ec
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-09-27 22:25:17 +0300

    a little text update on main form page

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -2,12 +2,11 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Forms</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
+      <v-btn text href="https://github.com/microbo-ru/hack_cifri_final" target="_blank">
+        <span class="mr-2">code</span>
       </v-btn>
     </v-app-bar>
 
