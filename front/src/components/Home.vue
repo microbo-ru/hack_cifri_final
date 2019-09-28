aadd55b3d1130cd7d1025a94699d6c02a297b1c7
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-09-28 13:22:50 +0300

    Home screen and new form elemnt in fromntend

---
+++ front/src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card class="mx-auto" width="344">
+          <v-card-text>
+            <p class="display-1 text--primary">Страница аналитика</p>
+
+            <div class="text--primary">Нужна для создания форм аналитиком</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text color="deep-purple accent-4" @click="$emit('NewForm')">Learn More</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="mx-auto" width="344">
+          <v-card-text>
+            <p class="display-1 text--primary">Страница Путенера</p>
+
+            <div class="text--primary">Нажна для создания логики обработки формы</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text color="deep-purple accent-4" @click="$emit('Routes')">Learn More</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="mx-auto" width="344">
+          <v-card-text>
+            <p class="display-1 text--primary">Страница пользователя</p>
+
+            <div class="text--primary">Нужна чтобы пользоваться благами</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text color="deep-purple accent-4" @click="$emit('UserForm')">Learn More</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
---
+++ front/src/components/NewFormPreview.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <FormGenerator :schema="form_schema" v-model="form_data" />
+    <v-btn @click="changeActiveForm">Change active from</v-btn>
+  </v-container>
+</template>
+
+<script>
+import FormGenerator from "./FormGenerator";
+
+export default {
+  name: "NewFormPreview",
+  data() {
+    return {
+      form_data: {}
+    };
+  },
+  computed: {
+    form_schema() {
+      return this.$store.getters.getActiveForm["schema"];
+    }
+  },
+  methods: {
+    changeActiveForm() {
+      let form_number = this.$store.state.activeFormNumber;
+      form_number = form_number == 0 ? 1 : 0;
+      this.$store.commit("changeActiveForm", { form_number });
+    }
+  },
+  watch: {
+    form_data: function() {}
+  },
+  components: {
+    FormGenerator
+  }
+};
+</script>
